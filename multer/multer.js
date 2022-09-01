f176f5767c062dc65bf5b2044ab0b3d4fecd6c3f
Alzhikkkk <alzhikakh@gmail.com>
2022-09-01 22:21:22 +0600

    Cors changes

--- multer/multer.js
+++ multer/multer.js
@@ -2,7 +2,7 @@ const multer  = require('multer')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         
-      cb(null, '../frontend/src/img/avatar')
+      cb(null, '../socket_telegrom_frontend/src/img/avatar')
     },
     filename: function (req, file, cb) {
        const filename = file.originalname;
